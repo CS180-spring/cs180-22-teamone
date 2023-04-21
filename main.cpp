ebc8d2268ce0b52a2351ef3ec83f7b9ede9fa72e
Het Patel <Hetpatel500@gmail.com>
2023-04-20 18:18:47 -0700

    update

--- main.cpp
+++ main.cpp
@@ -17,5 +17,77 @@ int main(){
     cout << "5. Exit" << endl;
 
 
-    
+    map<std::string, Json::Value> fields;
+    string id;
+    string field;
+    string value;
+    Record record;
+
+    while (true)
+    {
+        cout << "Enter your choice: "; 
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                cout << "Enter ID: ";
+                cin >> record.id;
+                
+                cout << "Enter field name (type exit to stop): ";
+
+                while(true){
+                    cin >> field;
+                    if(field == "exit"){
+                        break;
+                    }
+
+                    cout << "Enter value for " << field << ": ";
+                    cin >> value; 
+                    fields[field] = Json::Value(value);
+                }
+                record.fields = fields;
+                createRecord(record, filename);
+                break;
+
+            case 2:
+                cout << "Enter ID: ";
+                cid >> id;
+                readRecord(id, filename);
+                break;
+            
+            case 3:
+                cout << "Enter ID: ";
+                cin >> id; 
+                cout << "Enter field name (type exit to stop): ";
+                while(true){
+                    cin >> field;
+                    if(field == "exit"){
+                        break;
+                    }
+
+                    cout << "Enter value for " << field << ": ";
+                    cin >> value; 
+                    fields[field] = Json::Value(value);
+                }
+                updateRecord(id, fields, filename);
+                break;
+            
+            case 4:
+                cout << "Enter ID: ";
+                cin >> id; 
+                deleteRecord(id, filename);
+                break;
+            
+            case 5:
+                return 0;
+            
+            default:
+                cout << "Invalid choice." << std::endl;
+        }
+
+        fields.clear();
+        cout << endl;
+
+    }
+    return 0;
  }
