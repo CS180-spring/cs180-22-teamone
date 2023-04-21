c6fc08cba410e94c71be9c121d54b887f5a02b1b
dtalmood <dtalmood1@gmail.com>
2023-04-21 11:26:58 -0700

    Updated test.py

--- _test.py
+++ _test.py
@@ -2,6 +2,45 @@ import unittest
 
 from main import 
 
+class TestCreateRecord(unittest.TestCase):
+    def test_my_function(self):
+        #self.assertEqual(my_function(2), 4)
+        #self.assertEqual(my_function(0), 0)
+        #self.assertEqual(my_function(-1), -2)
+
+
+class TestGetFields(unittest.TestCase):
+    def test_my_function(self):
+        #self.assertEqual(my_function(2), 4)
+        #self.assertEqual(my_function(0), 0)
+        #self.assertEqual(my_function(-1), -2)
+
+
+class TestReadRecord(unittest.TestCase):
+    def test_my_function(self):
+        #self.assertEqual(my_function(2), 4)
+        #self.assertEqual(my_function(0), 0)
+        #self.assertEqual(my_function(-1), -2)
+
+class TestUpdateRecord(unittest.TestCase):
+    def test_my_function(self):
+        #self.assertEqual(my_function(2), 4)
+        #self.assertEqual(my_function(0), 0)
+        #self.assertEqual(my_function(-1), -2)
+
+class TestDeleteRecord(unittest.TestCase):
+    def test_my_function(self):
+        #self.assertEqual(my_function(2), 4)
+        #self.assertEqual(my_function(0), 0)
+        #self.assertEqual(my_function(-1), -2)
+
+class TestListRecords(unittest.TestCase):
+    def test_my_function(self):
+        #self.assertEqual(my_function(2), 4)
+        #self.assertEqual(my_function(0), 0)
+        #self.assertEqual(my_function(-1), -2)
+
+        
 
 if __name__ == '__main__':
     unittest.main()
