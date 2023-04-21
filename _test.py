3ec174a958b4c31e39bfac66eb17457e1461f3b1
dtalmood <dtalmood1@gmail.com>
2023-04-21 11:28:42 -0700

    Imported function from main.py

--- _test.py
+++ _test.py
@@ -1,6 +1,13 @@
 import unittest
 
-from main import 
+from main import create_record
+from main import get_fields
+from main import read_record
+from main import update_record
+from main import delete_record
+from main import list_records
+
+
 
 class TestCreateRecord(unittest.TestCase):
     def test_my_function(self):
@@ -40,7 +47,7 @@ class TestListRecords(unittest.TestCase):
         #self.assertEqual(my_function(0), 0)
         #self.assertEqual(my_function(-1), -2)
 
-        
+
 
 if __name__ == '__main__':
     unittest.main()
