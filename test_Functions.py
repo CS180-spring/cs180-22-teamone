b9a4cc50967ceaf20dc759ea5db5ac065c6b9213
Het Patel <HetPatel500@gmail.com>
2023-04-25 17:28:46 -0700

    initial testing setup

---
+++ test_Functions.py
@@ -0,0 +1,28 @@
+import unittest
+import Functions
+
+class TestFunctions(unittest.TestCase):
+    
+    
+    def test_create_record(self):
+        pass
+    
+    
+    def test_get_fields(self):
+        pass
+    
+    
+    def test_read_records(self):
+        pass
+    
+    
+    def test_update_record(self):
+        pass
+    
+    
+    def test_delete_record(self):
+        pass
+    
+    
+    def test_list_record(self):
+        pass
