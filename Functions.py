194ad0e8d1a24b9b2cdf3b30fdd46e9bdab42758
Het Patel <HetPatel500@gmail.com>
2023-05-11 19:15:31 -0700

    menu system in progress

--- Functions.py
+++ Functions.py
@@ -6,8 +6,6 @@ import os   # import the os module for operating system dependent functionality
 DB_FILE_NAME = ''  # define the name of the file where records will be stored
 EXISTING_DATA_BASES = []
 
-
-
 # Function to create records
 def create_record(): # option 5
     global DB_FILE_NAME
@@ -305,9 +303,117 @@ def listField():
          print(f"No data under the field name {userInput} was found")
 
         
-                
+def displayMenu(users):
+    print("LOGIN MENU")
+    print("1. Create User\n")
+    print("2. Log In")
+    choice = input("Please Select your option: ")
+    
+    if (choice == "1"):
+        username = input("Username: ")
+        
+        if username in users:
+            print("\nUsername Already Exists!\n")
+        else:
+            password = input("Password: ")
+            users[username] = password
+            print("User Created!")
+    if (choice == "2"):
+        username = input("Enter Username: ")
+        password = input("Enter Password: ")
+        
+        if username in users and users[username] == password:
+            print("\Login Successful\n")
+            mainMenu()
+        else:
+            print("\nUser Doesnt Exist // Wrong Password\n")                        
 
 
+def mainMenu():
+    while True:
+        
+        print('\nMenu')
+        print('1. Create a DataBase')
+        print('2. Current Database')
+        print('3. Choose Database')
+        print('4. Delete Database')
+        print('5. Create record')
+        print('6. Read Record')
+        print('7. Update record')
+        print('8. Delete record')
+        print('9. List records')
+        print ('10. List by field')
+        print('11. Create Database from CSV')
+        print('12. Search Database')
+        print('13. Search All Databases')
+        print('14. Quit')
+        
+        choice = input(' Enter choice: ')
+        
+        if choice == '1':
+            create_dataBase()
+
+        if choice == '2':
+            current_database()
+
+        if choice == '3':
+            choose_database()
+
+        if choice == '4':          
+            delete_database()
+
+        if choice == '5':          
+            create_record()
+        
+        elif choice == '6':
+            id = input('Enter ID: ')
+            record = read_record(id)
+            if record:
+                print(record)
+            else:
+                print('Record not found')
+        
+        elif choice == '7':
+            id = input("Enter ID: ")
+            if update_record(id):
+                print("Record updated")
+            else:
+                print("Record not found")
+        
+        elif choice == '8' :
+            id = input("Enter ID: ")
+            if delete_record(id):
+                print('Record deleted')
+            else:
+                print("Record not found")
+      
+        elif choice == '9':
+            list_records()
+        
+        elif choice =='10':
+            listField()
+
+        elif choice == '11':
+            create_databaseCSV()
+
+        elif choice == '12':
+            record = searchCurrentDatabase()
+            if record:
+                print(record)
+            else:
+                print("No Records Found!")
+
+        elif choice =='13':
+            record = searchThroughAllDatabases()
+            if record:
+                print(record)
+            else:
+                print("No Records Found!")
+        
+        
+        elif choice == '14':
+            
+            break
 
 
 
--- main.py
+++ main.py
@@ -2,91 +2,12 @@ import json
 import os
 from Functions import *
 
+users = {}
 
 def main():
     while True:
-        print('\nMenu')
-        print('1. Create a DataBase')
-        print('2. Current Database')
-        print('3. Choose Database')
-        print('4. Delete Database')
-        print('5. Create record')
-        print('6. Read Record')
-        print('7. Update record')
-        print('8. Delete record')
-        print('9. List records')
-        print ('10. List by field')
-        print('11. Create Database from CSV')
-        print('12. Search Database')
-        print('13. Search All Databases')
-        print('14. Quit')
+        displayMenu(users)
         
-        choice = input(' Enter choice: ')
-        
-        if choice == '1':
-            create_dataBase()
-
-        if choice == '2':
-            current_database()
-
-        if choice == '3':
-            choose_database()
-
-        if choice == '4':          
-            delete_database()
-
-        if choice == '5':          
-            create_record()
-        
-        elif choice == '6':
-            id = input('Enter ID: ')
-            record = read_record(id)
-            if record:
-                print(record)
-            else:
-                print('Record not found')
-        
-        elif choice == '7':
-            id = input("Enter ID: ")
-            if update_record(id):
-                print("Record updated")
-            else:
-                print("Record not found")
-        
-        elif choice == '8' :
-            id = input("Enter ID: ")
-            if delete_record(id):
-                print('Record deleted')
-            else:
-                print("Record not found")
-      
-        elif choice == '9':
-            list_records()
-        
-        elif choice =='10':
-            listField()
-
-        elif choice == '11':
-            create_databaseCSV()
-
-        elif choice == '12':
-            record = searchCurrentDatabase()
-            if record:
-                print(record)
-            else:
-                print("No Records Found!")
-
-        elif choice =='13':
-            record = searchThroughAllDatabases()
-            if record:
-                print(record)
-            else:
-                print("No Records Found!")
-        
-        
-        elif choice == '14':
-            
-            break
 
 if __name__ == '__main__':
     main()
